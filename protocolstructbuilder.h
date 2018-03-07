66801fe59ae5705fc53f5165e57f9946de2e3b24
toribasho <naotta@gmail.com>
2018-03-07 14:53:00 +0300

    remove struct declaration to the sreuctbuilderdata.h

--- protocolstructbuilder.h
+++ protocolstructbuilder.h
@@ -6,68 +6,8 @@
 #include <QString>
 #include <QtXml/QtXml>
 #include <QXmlStreamReader>
-
-enum containerType {typeList = 0};
-enum _objectType { SIMPLE = 0, STRUCT = 1, CONTAINER = 2};
-//enum _fieldType { typeBool, typeInt, typeString, typeDate, typeTime, typeDateTime};
-
-struct baseFieldObject{
-    virtual _objectType getObjectType() = 0;
-};
-
-struct fieldDbData{
-    QString _db_fieldName;
-    QString _value;
-    bool multiValue;
-    QString concat;
-};
-
-struct fieldObject: public baseFieldObject{
-
-    virtual _objectType getObjectType(){return _objectType::SIMPLE;}
-
-    QString _fieldType;
-    QString _fieldName;
-    QString _fieldFormat;
-    QString _fieldScale;
-    bool    required;
-
-    QString _hashKey;                           // key for state hash
-    QString _resultHashListKey;                 // key for value hash from state hash
-};
-
-struct structObject: public baseFieldObject{
-
-    virtual _objectType getObjectType(){return _objectType::STRUCT;}
-
-    QString _exStructName;                      // name as Type
-    QString _structAsFieldName;                 // variableName
-
-    QList< baseFieldObject *> _structFields;
-
-    QHash<QString, QList<fieldDbData> > dbData;
-};
-
-struct containerObject: public baseFieldObject{
-
-    virtual _objectType getObjectType(){return _objectType::CONTAINER;}
-
-    containerType _containerType;               // enum?
-    QString _exContainerName;                   // name as Type
-    QString _containerAsFieldName;              // variableName
-
-    QList< baseFieldObject *> _inlineData;      // structData or fieldData. todo: fix
-    baseFieldObject * _internalField;           // only one value can be in list. struct or field
-};
-
-struct structBuilderObject{
-    QString _structName;
-    QString _structParent;
-    QStringList _fieldsNames;
-    QList< baseFieldObject *> _structFields;
-
-    QHash<QString, QList<fieldDbData> > dbData;
-};
+#include <stdexcept>
+#include "structbuilderdata.h"
 
 class ProtocolStructBuilder
 {
@@ -77,7 +17,7 @@ class ProtocolStructBuilder
 
     void loadXmlData();
 //    fieldObject * makeFieldObject(const QXmlStreamReader &xml);
-    QString makeStructDeclaration(structObject *);
+    QByteArray makeStructDeclaration(structObject *);
     void createHeaderFile(const QString &fileName);
     void createSourceFile(const QString &fileName);
 
